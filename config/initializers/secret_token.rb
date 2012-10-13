12d53615799c3a5aeb05a0d80b962245c8d65517
miio <info@miio.info>
2012-10-13 23:42:55 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeMinesweeper::Application.config.secret_token = '4b787b76c3399e0aadbfb5268b8212c74cadba0d6c97673d77f2dcc9714a9f0eb1a09d58589cec6affd693f3d9d33d01f61c35c3e7b552c237dbc38629fa7a86'
